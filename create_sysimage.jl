38e1fd8701db1baa7db94f70c752baf8447835bd
Jakob Asslaender <jakob.asslaender@nyumc.org>
2021-04-08 13:43:10 -0400

    remove warmup.jl

--- create_sysimage.jl
+++ create_sysimage.jl
@@ -12,6 +12,6 @@ create_sysimage([
 :PlutoUI
 ];
 
-precompile_execution_file="warmup.jl",
+# precompile_execution_file="warmup.jl",
 replace_default=true,
 cpu_target = PackageCompiler.default_app_cpu_target())
